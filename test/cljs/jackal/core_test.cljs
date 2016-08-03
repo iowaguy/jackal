7d6c79d9ca5776bddcb853c5a21eb6f0f5697dec
Ben Weintraub <benweintraub34@gmail.com>
2016-08-02 19:56:38 -0700

    JACKAL-10: Improved performance in Mandelbrot functions. Removed most function calls, and optimized arithmatic. Also, added benchmarks and tests. Committed by Ben Weintraub.

--- test/cljs/jackal/core_test.cljs
+++ test/cljs/jackal/core_test.cljs
@@ -1,6 +1,13 @@
 (ns jackal.core-test
-  (:require-macros [cljs.test :refer (is deftest testing)])
-  (:require [cljs.test]))
+  (:require [cljs.test :refer-macros [deftest is testing run-tests]]
+            [jackal.math.numerics :as numerics]))
 
-(deftest example-passing-test
-  (is (= 1 1)))
+(deftest mandelbrot-set-test
+  (is (numerics/mandelbrot-set? 0 0 1000)))
+
+(deftest mandelbrot-set-negative-test
+  (is (not (numerics/mandelbrot-set? 1 1 1000))))
+
+(simple-benchmark [x 0 y 0 i 1000] (numerics/mandelbrot-set? x y i) 1000)
+(simple-benchmark [x 0 y 0 i 1000] (numerics/mandelbrot-set? x y i) 10000)
+(simple-benchmark [x 0 y 0 i 1000] (numerics/mandelbrot-set? x y i) 100000)

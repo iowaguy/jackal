d00d21a220089fa72650bc38a90fde25f16cfa8c
Ben Weintraub <benweintraub34@gmail.com>
2016-08-08 19:15:41 -0700

    Removed margins from html rendering. Changed tests to be more representative of actual operation. Committed by Ben Weintraub.

--- test/cljs/jackal/core_test.cljs
+++ test/cljs/jackal/core_test.cljs
@@ -3,14 +3,15 @@
             [jackal.math.numerics :as numerics]))
 
 (deftest mandelbrot-set-test
-  (is (numerics/mandelbrot-set? 0 0 1000)))
+  (is (numerics/mandelbrot-set? 0 0 50)))
 
 (deftest mandelbrot-set-negative-test
-  (is (not (numerics/mandelbrot-set? 1 1 1000))))
+  (is (not (numerics/mandelbrot-set? 1 1 50))))
 
-(simple-benchmark [x 0 y 0 i 1000] (numerics/mandelbrot-set-iterations x y i) 1000)
-(simple-benchmark [x 0 y 0 i 1000] (numerics/mandelbrot-set-iterations x y i) 10000)
-(simple-benchmark [x 0 y 0 i 1000] (numerics/mandelbrot-set-iterations x y i) 100000)
+(simple-benchmark [x 0 y 0 i 50] (numerics/mandelbrot-set-iterations x y i) 1000)
+(simple-benchmark [x 0 y 0 i 50] (numerics/mandelbrot-set-iterations x y i) 10000)
+(simple-benchmark [x 0 y 0 i 50] (numerics/mandelbrot-set-iterations x y i) 100000)
 
 ;; actual number of points in full screen
 (simple-benchmark [x 0 y 0 i 1000] (numerics/mandelbrot-set-iterations x y i) 2560000)
+(simple-benchmark [x 0 y 0 i 50] (numerics/mandelbrot-set-iterations x y i) 2560000)
